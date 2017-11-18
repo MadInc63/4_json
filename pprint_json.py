1e6e337a09154f8e4ffcb7181e257cf59824694a
MadInc63 <MadInc63@gmail.com>
2017-11-18 10:03:19 +0400

    надеюсь все )

--- pprint_json.py
+++ pprint_json.py
@@ -2,21 +2,23 @@ import json
 import sys
 
 
-def load_from_json_file(file_path):
+def load_json_file(file_path):
     try:
         with open(file_path, 'r') as json_file:
-            result_of_load = json.load(json_file)
-    except IOError:
+            json_content = json.load(json_file)
+        return json_content
+    except (IOError, FileNotFoundError):
         print('No such file or directory')
     except ValueError:
         print('File is empty')
-    else:
-        return result_of_load
 
 
-def pretty_print_json(json_data):
-    print(json.dumps(json_data, ensure_ascii=False, indent=4, sort_keys=True))
+def pretty_print_json(json_content_input):
+    print(json.dumps(json_content_input, ensure_ascii=False,
+                     indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_from_json_file(sys.argv[1]))
+    json_load = load_json_file(sys.argv[1])
+    if json_load is not None:
+        pretty_print_json(json_load)
